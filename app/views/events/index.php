e5e7d79797c3a2fc94e51767b8c883189851ba99
Chris Woudstra <chris@mygovonline.com>
2018-05-14 23:49:55 -0400

    styling changes

--- app/views/events/index.php
+++ app/views/events/index.php
@@ -1,12 +1,26 @@
 <?php require APP_ROOT . '/views/inc/header.php'; ?>
-    <div class="row">
-        <div class="col-md-6">
-            <h2 class="red">Events</h2>
+    <div class="row mb-4 pt-2">
+        <div class="col-md-6 offset-md-3">
+            <h2 class="red caps text-center">Events</h2>
         </div>
-        <div class="col-md-6">
-            <a href="<?=URL_ROOT?>/events/add" class="btn custom-red-btn pull-right">
-                <i class="fa fa-plus"></i> Add Event
+        <div class="col-md-6 offset-md-3">
+            <a href="<?=URL_ROOT?>/events/add" class="btn custom-red-btn pull-right mx-auto">
+                <i class="fa fa-plus"></i> Add
             </a>
         </div>
+    </div>
+    <div class="row">
+        <?php foreach($data['events'] as $event) : ?>
+        <div class="col-6 offset-md-3">
+            <div class="card card-body mb-3 custom-container">
+                <h5 class="card-title red text-center"><?=$event->name?></h5>
+                <h6 class="text-center gray pb-1"><?=$event->foundation?></h6>
+                <p class="card-body gray"><?=$event->description?></p>
+                <div class="custom-btn-style mx-auto">
+                    <a href="<?=URL_ROOT?>/events/show/<?=$event->id?>" class="btn custom-red-btn">More</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
     </div>
 <?php require APP_ROOT . '/views/inc/footer.php'; ?>
