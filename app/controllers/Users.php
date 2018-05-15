8cbf25c36380b326f006033e2dd429923fb53eb9
Chris Woudstra <chris@mygovonline.com>
2018-05-15 06:58:12 -0400

    redirect functionality

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -150,5 +150,6 @@ class Users extends Controller {
         unset($_SESSION['user_email']);
         unset($_SESSION['user_name']);
         session_destroy();
+        redirect('users/login');
     }
 }
