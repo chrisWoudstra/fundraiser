0a3e90caa2e9bf12c13bdc21b5b3405ea445c6cf
Chris Woudstra <chris@mygovonline.com>
2018-05-14 21:38:21 -0400

    moving isLoggedIn to helper function

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -9,14 +9,10 @@ class Users extends Controller {
     }
 
     public function register(){
-        // Check for POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-            // Process form
 
-            // Sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-            // Init data
             $data =[
                 'name' => trim($_POST['name']),
                 'email' => trim($_POST['email']),
@@ -28,7 +24,6 @@ class Users extends Controller {
                 'confirm_password_err' => ''
             ];
 
-            // Validate Email
             if (empty($data['email'])){
                 $data['email_err'] = 'Please enter email';
             } else {
@@ -37,19 +32,16 @@ class Users extends Controller {
                 }
             }
 
-            // Validate Name
             if (empty($data['name'])){
                 $data['name_err'] = 'Please enter name';
             }
 
-            // Validate Password
             if (empty($data['password'])){
                 $data['password_err'] = 'Please enter password';
             } elseif(strlen($data['password']) < 6){
                 $data['password_err'] = 'Password must be at least 6 characters';
             }
 
-            // Validate Confirm Password
             if (empty($data['confirm_password'])){
                 $data['confirm_password_err'] = 'Please confirm password';
             } else {
@@ -58,7 +50,6 @@ class Users extends Controller {
                 }
             }
 
-            // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
                 // hash password
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
@@ -72,12 +63,11 @@ class Users extends Controller {
 
 
             } else {
-                // Load view with errors
                 $this->view('users/register', $data);
             }
 
         } else {
-            // Init data
+
             $data =[
                 'name' => '',
                 'email' => '',
@@ -89,19 +79,15 @@ class Users extends Controller {
                 'confirm_password_err' => ''
             ];
 
-            // Load view
             $this->view('users/register', $data);
         }
     }
 
     public function login(){
-        // Check for POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-            // Process form
-            // Sanitize POST data
+
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-            // Init data
             $data =[
                 'email' => trim($_POST['email']),
                 'password' => trim($_POST['password']),
@@ -140,7 +126,7 @@ class Users extends Controller {
 
 
         } else {
-            // Init data
+
             $data =[
                 'email' => '',
                 'password' => '',
@@ -148,7 +134,6 @@ class Users extends Controller {
                 'password_err' => '',
             ];
 
-            // Load view
             $this->view('users/login', $data);
         }
     }
@@ -157,7 +142,7 @@ class Users extends Controller {
         $_SESSION['user_id'] = $user->id;
         $_SESSION['user_email'] = $user->email;
         $_SESSION['user_name'] = $user->name;
-        redirect('users/login');
+        redirect('events/index');
     }
 
     public function logout() {
@@ -165,13 +150,5 @@ class Users extends Controller {
         unset($_SESSION['user_email']);
         unset($_SESSION['user_name']);
         session_destroy();
-    }
-
-    public function isLoggedIn() {
-        if(isset($_SESSION['user_id'])) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
