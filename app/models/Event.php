2a16ca07a4c204c07fbb9634687ccc673f0708a3
Chris Woudstra <chris@mygovonline.com>
2018-05-15 21:49:00 -0400

    more functionality

--- app/models/Event.php
+++ app/models/Event.php
@@ -9,7 +9,12 @@ class Event {
     }
 
     public function getEvents() {
-        $this->db->query('SELECT * FROM events');
+        $this->db->query("SELECT E.*,
+                                     (SELECT (sum(rating)/count(rating)) 
+                                      FROM reviews 
+                                      WHERE event_id = E.id) as avgRating
+                              FROM events E
+                              ORDER BY avgRating DESC");
 
         return $this->db->resultSet();
     }
@@ -30,10 +35,57 @@ class Event {
         }
     }
 
+    public function addReview($data) {
+
+        $this->db->query('INSERT INTO reviews (event_id, user_id, rating, review, created_at) 
+                              VALUES (:event_id, :user_id, :rating, :review, NOW())');
+
+        $this->db->bind(':event_id', intval($data['event_id']));
+        $this->db->bind(':user_id', $_SESSION['user_id']);
+        $this->db->bind(':rating', intval($data['rating']));
+        $this->db->bind(':review', $data['review']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function getEventById($id) {
-        $this->db->query('SELECT * FROM events WHERE id= :id');
+        $this->db->query('SELECT * FROM events WHERE id = :id');
+
         $this->db->bind(':id', $id);
 
         return $this->db->single();
+    }
+
+    public function getReviews($eventId) {
+        $this->db->query("SELECT R.rating,
+                                     R.review,
+                                     U.name,
+                                     DATE_FORMAT(R.created_at, '%M %D, %Y') as reviewDate
+                              FROM reviews R 
+                                INNER JOIN users U on R.user_id = U.id
+                              WHERE R.event_id = :event_id
+                              ORDER BY R.created_at DESC");
+
+        $this->db->bind(':event_id', $eventId);
+
+        return $this->db->resultSet();
+    }
+
+    public function userHasNotAlreadyReviewed($eventId) {
+
+        $this->db->query("SELECT * FROM reviews WHERE event_id = :event_id AND user_id = :user_id");
+
+        $this->db->bind(':event_id', $eventId);
+        $this->db->bind(':user_id', $_SESSION['user_id']);
+
+        if (empty($this->db->single())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
