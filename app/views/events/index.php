02d24dec5a2e32b08cf1c73f6165bc5052631bbe
Chris Woudstra <chris@mygovonline.com>
2018-05-15 21:50:16 -0400

    styling and show single event

--- app/views/events/index.php
+++ app/views/events/index.php
@@ -1,12 +1,14 @@
 <?php require APP_ROOT . '/views/inc/header.php'; ?>
     <?php flash('event_message'); ?>
-    <div class="row mb-4 pt-2">
+    <div class="row mb-2 pt-2">
         <div class="col-md-6 offset-md-3">
             <h2 class="red caps text-center">Events</h2>
         </div>
-        <div class="col-md-6 offset-md-3">
-            <a href="<?=URL_ROOT?>/events/add" class="btn custom-red-btn pull-right mx-auto">
-                <i class="fa fa-plus"></i> Add
+    </div>
+    <div class="row pb-4">
+        <div class="col-md-6 offset-md-3 text-center">
+            <a href="<?=URL_ROOT?>/events/add" class="btn custom-red-btn">
+                <i class="fa fa-plus"></i> Add Event
             </a>
         </div>
     </div>
@@ -16,6 +18,7 @@
                 <div class="card card-body mb-3 custom-container">
                     <h5 class="card-title red text-center"><?=$event->name?></h5>
                     <h6 class="text-center gray pb-1"><?=$event->foundation?></h6>
+                    <p class="text-center gray mb-1">Avg Rating:&nbsp;&nbsp;<span class="red"><?=number_format($event->avgRating,1)?></span></p>
                     <p class="card-body gray"><?=$event->body?></p>
                     <div class="custom-btn-style mx-auto text-center">
                         <a href="<?=URL_ROOT?>/events/show/<?=$event->id?>" class="btn custom-red-btn">More</a>
