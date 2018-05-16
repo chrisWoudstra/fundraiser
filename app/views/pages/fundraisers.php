193379f1d47ab9104147eb0e150c392c1a09f637
Chris Woudstra <chris@mygovonline.com>
2018-05-15 21:46:11 -0400

    styling the home page

--- app/views/pages/fundraisers.php
+++
@@ -1,3 +0,0 @@
-<?php require APP_ROOT . '/views/inc/header.php'; ?>
-    <h1>Fundraiser List</h1>
-<?php require APP_ROOT . '/views/inc/footer.php'; ?>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,19 @@
 <?php require APP_ROOT . '/views/inc/header.php'; ?>
-    <div class="jumbotron text-center">
-        <div class="container">
-            <h1 class="display-3"><?=$data['title']?></h1>
-            <p class="lead"><?=$data['description']?></p>
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <div class="card card-body mb-3 custom-container">
+                <h2 class="card-title red text-center">Welcome!</h2>
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h5 class="card-body gray">Here you can <span class="red">view</span>,
+                        <span class="red">rate</span>, <span class="red">review</span>,
+                        and even <span class="red">add</span> your favorite fundraiser event!
+                        Please register and login to continue!
+                    </h5>
+                    <div class="custom-btn-style mx-auto text-center">
+                        <a href="<?=URL_ROOT?>/users/register" class="btn custom-red-btn">Register</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 <?php require APP_ROOT . '/views/inc/footer.php'; ?>
