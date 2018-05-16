3b126e5e3d4868007a7993fa717114655dd528c0
Chris Woudstra <chris@mygovonline.com>
2018-05-15 21:49:43 -0400

    allowing the user to close out the flash messages

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -17,7 +17,9 @@ function flash($name = '', $message = '', $class = 'alert alert-success') {
             $_SESSION[$name . '_class'] = $class;
         } elseif (empty($message) && !empty($_SESSION[$name])) {
             $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
-            echo '<div class="' . $class . ' text-center" id="msg-flash">' . $_SESSION[$name] . '</div>';
+            echo '<div class="' . $class . ' text-center" id="msg-flash">' . $_SESSION[$name] .
+                  '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' .
+                  '<span aria-hidden="true">&times;</span></button></div>';
             unset($_SESSION[$name]);
             unset($_SESSION[$name . '_class']);
         }
